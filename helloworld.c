255e8f947d5535aaf3f67c70e768fb41c355ca4e
Arnav Patil <arnavpatil@Arnavs-MacBook-Air.local>
2021-05-18 18:33:42 +0530

    added hello world.c

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+// This the most simple program written in c
+#include<stdio.h>
+//main function "enter point of the program"
+int main()
+{
+    //calling print function and printning hello world
+    printf("hello world\n");
+
+
+}
