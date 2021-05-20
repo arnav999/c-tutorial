37805ec8cf9f14c1b25820fba2ce04fb8d66bc95
Arnav Patil <arnavpatil@Arnavs-MacBook-Air.local>
2021-05-20 16:09:16 +0530

    added division.c

---
+++ basics/division.c
@@ -0,0 +1,10 @@
+//program to implement division of 2 numbers
+#include<stdio.h>
+void main(){
+  int a,b,c;
+  printf("enter value of a and b\n");
+  scanf("%d%d",&a,&b);
+  //division of two numbers
+  c = a/b;
+  printf("quotient of %d and %d is %d",a,b,c);
+}
