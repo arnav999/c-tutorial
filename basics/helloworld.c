5873d748ecdf4d2d4bb6a38eebbade1aac8083e5
Arnav Patil <56063843+arnav999@users.noreply.github.com>
2021-05-20 15:34:25 +0530

    Create helloworld.c

---
+++ basics/helloworld.c
@@ -0,0 +1,8 @@
+// this is a simple program to print hello world
+//including header file
+#include<stdio.h>
+//main function "enter point of program"
+int main(){
+  //print function for printing message
+  printf("hello world"\n);
+}
