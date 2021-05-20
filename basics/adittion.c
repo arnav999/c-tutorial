114049c517f50434a9ca88555871dde010690748
Arnav Patil <56063843+arnav999@users.noreply.github.com>
2021-05-20 15:41:28 +0530

    Create adittion.c

---
+++ basics/adittion.c
@@ -0,0 +1,13 @@
+//program for addition of 2 numbers
+#include<stdio.h>
+int main(){
+  //declearation of variables
+  int a,b,c;
+  printf("enter the value of a and b\n");
+  //scanf function for taking input
+  scanf("%d%d",&a,&b);
+  //addition of 2 numbers
+  c = a+b;
+  printf("addition of %d and %d is %d\n",a,b,c);
+}
+  
