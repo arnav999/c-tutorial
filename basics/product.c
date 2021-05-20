be2177987ec84710dd98d8d68d5eebe1e8276b44
Arnav Patil <56063843+arnav999@users.noreply.github.com>
2021-05-20 15:51:17 +0530

    Create product.c

---
+++ basics/product.c
@@ -0,0 +1,9 @@
+//program to find product of two numbers
+#include<stdio.h>
+void main(){
+  int a,b,c;
+  printf("enter value of a and b\n");
+  scanf("%d%d",&a,&b);
+  //product of two numbers
+  c = a*b;
+  printf("product of %d and %d is %d",a,b,c);
