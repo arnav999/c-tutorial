83018f273d463be6425e258e23179b9b8e5cc800
Arnav Patil <56063843+arnav999@users.noreply.github.com>
2021-05-20 15:47:14 +0530

    Create subtraction.c

---
+++ basics/subtraction.c
@@ -0,0 +1,10 @@
+//program for subtraction of two numbers
+#include<stdio.h>
+void main(){
+  int a,b,c;
+  printf("enter values of a and b\n");
+  scanf("%d%d",&a,&b);
+  //subtraction of 2 numbers
+  c = a-b;
+  printf("difference of %d and %d is %d",a,b,c);
+}
