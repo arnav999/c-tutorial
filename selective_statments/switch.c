79d397ef1f4a9d30517fbf937518b495823a8a4b
Arnav Patil <arnavpatil@Arnavs-MacBook-Air.local>
2021-05-21 10:06:49 +0530

    modified switch.c

---
+++ selective_statments/switch.c
@@ -0,0 +1,52 @@
+// program to build a simple calculator using switch statment
+
+#include<stdio.h>
+int main(){
+
+    char choice;
+    int a,b,c;
+    printf("enter '+' for addition function\n");
+    printf("enter '-' for subtraction function\n"); 
+    printf("enter '*' for multiplication function\n");
+    printf("enter '/' for division function\n");
+    printf("enter your choice\n");
+    scanf("%c",&choice);
+
+    switch (choice)
+    {
+    case '+':
+        printf("you have chosen addition\n");
+        printf("enter the value of a and b\n");
+        scanf("%d%d",&a,&b);
+        c = a+b;
+        printf("sum of %d and %d is %d",a,b,c);
+        break;
+
+        case '-':
+        printf("you have chosen subtraction\n");
+        printf("enter the value of a and b\n");
+        scanf("%d%d",&a,&b);
+        c = a-b;
+        printf("difference of %d and %d is %d",a,b,c);
+        break;
+
+        case '*':
+        printf("you have chosen multiplication\n");
+        printf("enter the value of a and b\n");
+        scanf("%d%d",&a,&b);
+        c = a*b;
+        printf("product of %d and %d is %d",a,b,c);
+        break;
+
+        case '/':
+        printf("you have chosen division\n");
+        printf("enter the value of a and b\n");
+        scanf("%d%d",&a,&b);
+        c = a/b;
+        printf("quotient of %d and %d is %d",a,b,c);
+        break;
+    
+    default:
+        break;
+    }
+}
